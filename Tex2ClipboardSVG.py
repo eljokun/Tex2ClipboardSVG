3f5d7b07a484a92a4cda1b9bfde22e277b2180aa
eljokun <93293178+eljokun@users.noreply.github.com>
2024-04-10 20:05:09 +0200

    Code rewritten entirely, added support for custom DPI and font size if possible, blank equations will no longer cause errors, but render an empty space by passing on '\text{ }' as equation

--- Tex2ClipboardSVG.py
+++ Tex2ClipboardSVG.py
@@ -1,6 +1,9 @@
 # TESTED IN PYTHON 3.12, CANNOT GUARANTEE COMPATIBILITY WITH OLDER VERSIONS
+# If anything happens, github.com/SolarBakha is to be blamed, even though the code is my creation
+# go blame her, she introduced the eldritch horror that are classes into this program,
+# even though they were the most unnecessary thing in the universe after what my ex did to me
 from PyQt6.QtSvgWidgets import QGraphicsSvgItem
-from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QVBoxLayout, QLineEdit
+from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QVBoxLayout, QLineEdit, QPushButton
 from PyQt6.QtGui import QGuiApplication
 from PyQt6.QtCore import QMimeData, QByteArray, Qt, QObject
 from PyQt6.QtWidgets import QGraphicsView, QGraphicsScene, QLabel
@@ -9,165 +12,210 @@ import sys
 import io
 import matplotlib.pyplot as plot
 
-# Declare that we're using math text for matplotlib
+# Declare math text usage for matplotlib
 plot.rc('mathtext', fontset='cm')
 
-# Prepare byte I/O stream for image data to be transferred to clipboard
-
-
+# Initialize properties class
 class Properties(QObject):
     def __init__(self):
         super().__init__()
-        self.byteIO = io.BytesIO()  # SVG
+        # Initialize byte I/O stream for svg data
+        self.byteIO = io.BytesIO()
 
         # Declare defaults
         self.DPI = 300
-        self.FONT_SIZE = 12
+        self.FontSize = 12
+        self.copyFlag = 0
 
 class MainWindow(QMainWindow):
+    # Define updaters
+    def updateDPILabel(self,flag):
+        # Update DPI value shown on label
+        self.DPILabel.setText(f'Resolution: {self.properties.DPI} DPI')
+        # If the value change failed, set to red background to indicate defaults were applied
+        if flag == 'FAIL':
+            self.DPIbox.setStyleSheet("background-color: rgb(255,143,143)")
+        # If the value change was successful, set to green background to indicate success
+        if flag == 'PASS':
+            self.DPIbox.setStyleSheet("background-color: rgb(149,255,171)")
+        # Refresh the label to display the new changes
+        self.DPILabel.update()
+    def updateDPIValue(self):
+        # Attempt to update DPI value, if it fails, set to default value and
+        # pass the fail flag to the label updater
+        try:
+            self.properties.DPI = int(self.DPIbox.text())
+            self.updateDPILabel('PASS')
+        # If the value change fails, set to default value and pass the fail flag to the label updater
+        # i dont even know why the fuck it would fail besides invalid values, but hey whatever
+        except Exception as DPIChangeError:
+            self.properties.DPI = 300
+            self.updateDPILabel('FAIL')
+            print(f'Failed to update DPI value to {self.DPIbox.text()} \n {DPIChangeError}')
+
+    def updateFontSizeLabel(self,flag):
+        # Change font size label text
+        self.FontSizeLabel.setText(f'Font Size: {self.properties.FontSize} pt')
+        # If change had failed, update with failure background
+        if flag == 'FAIL':
+            self.FontSizeBox.setStyleSheet("background-color: rgb(255,143,143)")
+        # If change was successful, update with light green background
+        if flag == 'PASS':
+            self.FontSizeBox.setStyleSheet("background-color: rgb(149,255,171)")
+        self.FontSizeLabel.update()
+
+    def updateFontSizeValue(self):
+        # try because idk, what could even make it fail lmao
+        try:
+            self.properties.FontSize = int(self.FontSizeBox.text())
+            # if it passes, update the label with a pass flag
+            self.updateFontSizeLabel('PASS')
+        except Exception as FontSizeError:
+            # if it fails, set to default value and update the label with a fail flag
+            self.properties.FontSize = 300
+            self.updateFontSizeLabel('FAIL')
+            # print the error, because i like complaining
+            print(f'Failed to update font size value: {self.FontSizeBox.text()} \n {FontSizeError}')
+    # Equation Renderer
+    def render_eq(self):
+        # Initialize plot
+        img = plot.figure(figsize=(1, 1))
+        # Retrieve Equation
+        eqn = self.eq_box.text()
+        # Strip spaces after it's done because matplotlib will bitch about a single space ._.
+        eqn = eqn.rstrip()
+        # If it's empty, render empty space to avoid errors
+        if eqn == '':
+            eqn = r'\text{ }'
+        try:
+            # Render the equation using matplotlib plot labelling
+            img.text(0, 0, fr'${eqn}$', fontsize=int(self.properties.FontSize), parse_math=True)
+            # Create SVG of plot, save as byte stream instead of file
+            img.savefig(self.properties.byteIO, transparent=True, dpi=int(self.properties.DPI), format='svg', bbox_inches='tight', pad_inches=0.1)
+        except ValueError:
+            pass
+        except Exception as rendererr:
+            raise Exception(f'Failed to render equation, error trace below \n {rendererr}')
+        finally:
+            plot.close(img)
+    # Retrieve SVG byte array and send to clipboard with MIME data
+    def createSVG(self):
+        # Retrieve SVG byte array
+        byte_array = QByteArray(self.properties.byteIO.getvalue())
+        # Initialize mime data as qmimedata object, thanks qt
+        mimeData = QMimeData()
+        # Set the data type to SVG+xml, with byte array as following data
+        mimeData.setData("image/svg+xml", byte_array)
+        # Initialize clipboard
+        clipboard = QGuiApplication.clipboard()
+        # Indicate mime type to clipboard
+        clipboard.setMimeData(mimeData)
+
+        # Write the SVG data to a temporary file, delete flag indicates it will be deleted after usage
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".svg") as temp:
+            # Write the byte array to the temporary file
+            temp.write(byte_array.data())
+            # Get the path of the temporary file
+            temp_path = temp.name
+        # Remove previous SVG from view
+        self.scene.clear()
+        # Load the temporary file created earlier
+        # into the QGraphicsSvgItem and add it to the viewport
+        svgItem = QGraphicsSvgItem(temp_path)
+        # Add new SVG
+        self.scene.addItem(svgItem)
+        # make it fit, duh
+        self.view.fitInView(svgItem, Qt.AspectRatioMode.KeepAspectRatio)
+        # Flush IO stream for next SVG byte array
+        self.properties.byteIO.seek(0)
+        self.properties.byteIO.truncate()
+
+    # Tick Rendering Per Keystroke
+    def renderticker(self):
+        # Fail flag to indicate if rendering failed
+        failFlag=0
+        # Attempt to render equation
+        try:
+            self.render_eq()
+        # If it fails set flag to one and print the error with the attempted equation
+        except Exception as eqnerr:
+            failFlag = 1
+            print(f'Failed at rendering equation {self.eq_box.text()}, trace below; \n {eqnerr}')
+        # If it passed, attempt to create and copy the SVG
+        if failFlag == 0:
+            try:
+                self.createSVG()
+            # If it fails, for some ungodly reason, print error message and trace info
+            except Exception as err:
+                print(f'Erorr: Could not create and copy SVG, information below; \n {err}')
+        else:
+            return
+
     def __init__(self):
         super().__init__()
+        # Assign properties
         self.properties = Properties()
 
+        # Initialize elements
+        # Create equation input field
+        self.eq_box = QLineEdit(self)
+        self.eq_box.setPlaceholderText("Input Equation [TeX]")
+        self.eq_box.textChanged.connect(self.renderticker)
 
-        self.text_box = QLineEdit(self)
-        self.text_box.setPlaceholderText("Input Equation [TeX]")
-        self.text_box.textChanged.connect(self.renderticker)
-        self.dpi_text_box = QLineEdit(self)
-        self.dpi_text_box.setPlaceholderText("DPI - Default is 300 DPI")
-        self.dpi_text_box.textChanged.connect(self.renderticker)
-        self.fontsize_text_box = QLineEdit(self)
-        self.fontsize_text_box.setPlaceholderText("Font Size [pt] - Default is 12")
-        self.fontsize_text_box.textChanged.connect(self.renderticker)
+        # Create DPI input field
+        self.DPIbox = QLineEdit(self)
+        # Set placeholder text
+        self.DPIbox.setPlaceholderText(f'DPI - Defaults to 300')
+        # Connect text change events to ticker and updates
+        self.DPIbox.textChanged.connect(self.updateDPIValue)
+        self.DPIbox.textChanged.connect(self.renderticker)
+
+        # Create Font Size input field
+        self.FontSizeBox = QLineEdit(self)
+        # Set placeholder text
+        self.FontSizeBox.setPlaceholderText(f'Font size - Defaults to 12 pt')
+        # Connect text change events to ticker and updates
+        self.FontSizeBox.textChanged.connect(self.updateFontSizeValue)
+        self.FontSizeBox.textChanged.connect(self.renderticker)
+
+        # Create DPI label
         self.DPILabel = QLabel(f'Resolution: {self.properties.DPI} DPI')
-        self.fontsizelabel = QLabel(f'Font size: {self.properties.FONT_SIZE} pt')
+        # Enable updates for the label for changes to show
+        self.DPILabel.setUpdatesEnabled(True)
+
+        # Create FontSize label
+        self.FontSizeLabel = QLabel(f'Font Size: {self.properties.FontSize} pt')
+        # Enable updates for the label for changes to show
+        self.FontSizeLabel.setUpdatesEnabled(True)
+
+        # Set window title and size
         self.setWindowTitle('Tex2ClipboardSVG')
         self.resize(800, 600)
+
+        # Create scene and view for SVG display
         self.scene = QGraphicsScene(self)
         self.view = QGraphicsView(self.scene, self)
+
+        # Create layout
         layout = QVBoxLayout()
-        layout.addWidget(self.text_box)
-        layout.addWidget(self.dpi_text_box)
-        layout.addWidget(self.fontsize_text_box)
+        layout.addWidget(self.eq_box)
+        layout.addWidget(self.DPIbox)
+        layout.addWidget(self.FontSizeBox)
         layout.addWidget(self.DPILabel)
-        layout.addWidget(self.fontsizelabel)
+        layout.addWidget(self.FontSizeLabel)
         layout.addWidget(self.view)
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
-        self.render_equation(
-            r'\iint_{T} \; \left| \dfrac{\partial \vec{v}(t,s)}{\partial t} \times '
-            r'\dfrac{\partial \vec{v}(t,s)}{\partial s}\right|')
 
-
-    def render_equation(self, equation):
-        # # Get byte IO as global duh
-        # global byteIO
-        # # get global user-defined or default DPI and font size
-        # global DPI
-        # global FONT_SIZE
-        # # matplotlib stuff lol
-        try:
-            # Initialize plot
-
-            img = plot.figure(figsize=(1, 1))
-            img.text(0, 0, fr"${equation.rstrip()}$", parse_math=True)
-
-            # Create SVG of plot, save as byte stream instead of file
-            img.savefig(self.properties.byteIO, dpi= self.properties.DPI, transparent=True, format='svg', bbox_inches='tight', pad_inches=0.1)
-            plot.close(img)
-        except Exception:
-            import traceback
-            traceback.print_exc()
-    def renderticker(self):
-        eq = self.text_box.text()
-        if not eq: return  # make sure it's not empty
-        # global DPI
-        # global FONT_SIZE
-        try:
-            self.properties.DPI = int(self.dpi_text_box.text())
-            self.properties.FONT_SIZE = int(self.fontsize_text_box.text())
-        except ValueError:
-            self.properties.DPI = 300
-            self.properties.FONT_SIZE = 12
-        # Set to 1 if equation failed to render, for safe display
-        failFlag = 0
-        try:
-            self.render_equation(eq)
-        except ValueError:
-            print(f'Error in equation: {eq}')
-        except Exception as e:
-            if e == 'FAILED':
-                failFlag = 1
-                self.render_equation(r'\text{ }')
-        # global byteIO
-        if failFlag == 0:
-            byte_array = QByteArray(self.properties.byteIO.getvalue())
-            mimeData = QMimeData()
-            mimeData.setData("image/svg+xml", byte_array)
-            clipboard = QGuiApplication.clipboard()
-            clipboard.setMimeData(mimeData)
-
-            # Write the SVG data to a temporary file
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".svg") as temp:
-                temp.write(byte_array.data())
-                temp_path = temp.name
-
-            # Load the temporary file into the QGraphicsSvgItem and add it to the scene
-            svgItem = QGraphicsSvgItem(temp_path)
-            self.scene.clear()  # Clear the previous SVG
-            self.scene.addItem(svgItem)
-            self.view.fitInView(svgItem, Qt.AspectRatioMode.KeepAspectRatio)  # Keep the aspect ratio
-
-            self.properties.byteIO.seek(0)
-            self.properties.byteIO.truncate()
-
-
+# main function because yes, if you dont know what that is i suggest you reconsider your life choices
 if __name__ == '__main__':
+    # Initialize Qt application
     app = QApplication(sys.argv)
+    # Initialize main window
     window = MainWindow()
+    # Show main window, because that is literally the point of the program
     window.show()
+    # Close when windows is closed because why the fuck would the CLI stay open
     sys.exit(app.exec())
-
-''' IGNORE: REWRITING THE ENTIRE PROGRAM AS A TEST. EXPERIMENTAL
-from PyQt6.QtSvgWidgets import QGraphicsSvgItem
-from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QVBoxLayout, QLineEdit
-from PyQt6.QtGui import QGuiApplication
-from PyQt6.QtCore import QMimeData, QByteArray, Qt, QObject
-from PyQt6.QtWidgets import QGraphicsView, QGraphicsScene, QLabel
-import tempfile
-import sys
-import io
-import matplotlib.pyplot as plot
-
- # u should have this in a separate git branch
- # well self is important in python, it the way to access class props
- # cuz rn you're just using globul variables which is catastrophically bad
- # so what else u want me to help with then
- # will check for any other problems
- # and more classes, spreading things out gut
- # you have everything in mainWindow
- # preferably, we'd have a class that's just the ui, one that's the props, and one that connects everying
- # you want more py files? DON'T MIND IF I DO
- # 
- # no it wouldn't, just need to get used to dir structure
- # then it better be good
- # also i  thiinkk im going to copy gwen directory structure and stuff
- # maybe the build tooling too so it's QUICK AND EASY so noobs don't have to install python, just run exe
- # how's that?
- 
- 
- more files would be neater tbh
- # we could refactor with ur self shit i guess
- # reminder this is for academics or note takers who just want a quick equation to be rendered
-def renderTeX(equation):
-    global DPI
-class MainWindow(QMainWindow):
-    DPI = 300
-    FONT_SIZE = 12
-
-
-
-
-'''
 
