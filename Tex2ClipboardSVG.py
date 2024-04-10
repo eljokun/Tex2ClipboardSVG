aaea570f12ef390a639183d482ac261df942c906
eljokun <93293178+eljokun@users.noreply.github.com>
2024-04-10 20:27:47 +0200

    I hate myself.

--- Tex2ClipboardSVG.py
+++ Tex2ClipboardSVG.py
@@ -112,7 +112,7 @@ class MainWindow(QMainWindow):
         clipboard.setMimeData(mimeData)
 
         # Write the SVG data to a temporary file
-        with tempfile.NamedTemporaryFile(delete_on_close=True, suffix=".svg") as temp:
+        with tempfile.NamedTemporaryFile(delete=False,delete_on_close=True, suffix=".svg") as temp:
             # Write the byte array to the temporary file
             temp.write(byte_array.data())
             # Get the path of the temporary file
